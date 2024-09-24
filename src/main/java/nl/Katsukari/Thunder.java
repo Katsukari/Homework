6a7beac47ee6a0cd648303713ad9361f5d054a5f
Katsukari <deborahkittenpaw@gmail.com>
2024-09-24 10:43:17 +0200

    Thunder exercise: put the unit of measurement prompt in the loop. Added seperate if statement if unit of measurement is invalid

--- src/main/java/nl/Katsukari/Thunder.java
+++ src/main/java/nl/Katsukari/Thunder.java
@@ -8,12 +8,18 @@ public class Thunder {
         final double speed_km = 0.343;  // Speed of sound in km 343 meter/s
         final double speed_feet = 1125.33;  // Speed of sound in feet/s
 
-        // Ask user for preferred unit of measurement
-        System.out.print("Would you like to calculate the distance in kilometers or feet? (km/feet): ");
-        String unit = scanner.next();
-
         // Loop to keep asking for seconds between lightning and thunder
         while (true) {
+            // Ask user for preferred unit of measurement
+            System.out.print("Would you like to calculate the distance in kilometers or feet?: ");
+            String unit = scanner.next();
+
+            // If measurement is invalid, let the user know and choose again
+            if (!unit.equals("km") && !unit.equals("feet")) {
+                System.out.println("Invalid unit. Please choose either 'km' or 'feet'.");
+                continue;
+            }
+
             System.out.print("Enter the seconds between lightning and thunder (or type a negative number to quit): ");
             int seconds = scanner.nextInt();
 
@@ -25,16 +31,13 @@ public class Thunder {
 
             double distance;
             double speed;
-            // if-else ladder depending on input
+            // if-else depending on input
             if (unit.equals("km")) {
                 distance = seconds * speed_km;
                 speed = speed_km;
-            } else if (unit.equals("feet")) {
+            } else {
                 distance = seconds * speed_feet;
                 speed = speed_feet;
-            } else {
-                System.out.println("Invalid unit. Please choose either 'km' or 'feet'.");
-                continue;
             }
             // Show output and let the user know how fast it's going, when it'll reach the user and distance to user
             System.out.println("The lightning is approximately " + distance + " " + unit + " away.");
